393f1970d1af037861086ba40c57499acb3ee252
Kit3l <pchumbakitel@gmail.com>
2023-01-08 22:45:47 -0800

    zero binary fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,17 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Write a function that creates a binary tree node
- * @parent: this pointer parent binary trees
- * @value: this value new node
- * Return: new node
+ * binary_tree_node - Create a binary tree node
+ * @parent: pointer to the parent node
+ * @value: value to store in the new node
+ * Return: pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-
-	if (value == '\0')
-		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
